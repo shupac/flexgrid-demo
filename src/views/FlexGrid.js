c19611c7188721417725e8680c31579c2961bf20
Hongxu Liu <djshu.us@gmail.com>
2014-10-21 15:11:22 -0700

    Animating state change using modifier

--- src/views/FlexGrid.js
+++ src/views/FlexGrid.js
@@ -1,10 +1,17 @@
 define(function(require, exports, module) {
     var View = require('famous/core/View');
     var Entity = require('famous/core/Entity');
+    var Modifier = require('famous/core/Modifier');
     var Transform = require('famous/core/Transform');
+    var Transitionable = require('famous/transitions/Transitionable');
+    var TransitionableTransform = require('famous/transitions/TransitionableTransform');
+    var Easing = require('famous/transitions/Easing');
 
     function FlexGrid() {
         View.apply(this, arguments);
+
+        this._modifiers = [];
+        this._states = [];
 
         this.id = Entity.register(this);
     }
@@ -17,7 +24,8 @@ define(function(require, exports, module) {
         marginSide: undefined,
         gutterCol: undefined,
         gutterRow: undefined,
-        itemSize: undefined
+        itemSize: undefined,
+        transition: { curve: Easing.outBack, duration: 500 }
     };
 
     function _calcSpacing(width) {
@@ -45,10 +53,7 @@ define(function(require, exports, module) {
         for (var i = 0; i < this._items.length; i++) {
             xPos = spacing.marginSide + col * spacing.ySpacing;
             yPos = this.options.marginTop + row * (this.options.itemSize[1] + this.options.gutterRow);
-            positions.push({
-                xPos: xPos,
-                yPos: yPos
-            });
+            positions.push([xPos, yPos, 0]);
 
             col++
             if (col === spacing.numCols) {
@@ -58,6 +63,24 @@ define(function(require, exports, module) {
         }
 
         return positions;
+    }
+
+    function _createModifier(index, position, size) {
+        var transitionItem = {
+            transform: new TransitionableTransform(Transform.translate.apply(null, position)),
+            size: new Transitionable((size || this.options.itemSize))
+        }
+
+        var modifier = new Modifier(transitionItem);
+
+        this._states[index] = transitionItem;
+        this._modifiers[index] = modifier;
+    }
+
+    function _animateModifier(index, position, size) {
+        var transformTransitionable = this._states[index].transform;
+        transformTransitionable.halt();
+        transformTransitionable.setTranslate(position, this.options.transition);
     }
 
     FlexGrid.prototype.sequenceFrom = function(items) {
@@ -78,20 +101,26 @@ define(function(require, exports, module) {
             var positions = _calcPositions.call(this, spacing);
 
             for (var i = 0; i < this._items.length; i++) {
-                var spec = {
-                    target: this._items[i].render(),
-                    size: this.options.itemSize,
-                    transform: Transform.translate(positions[i].xPos, positions[i].yPos, 0)
+                if (this._modifiers[i] === undefined) {
+                    _createModifier.call(this, i, positions[i]);
                 }
-
-                specs.push(spec);
+                else {
+                    _animateModifier.call(this, i, positions[i]);
+                }
             }
 
             this._cachedWidth = width;
-            this._cachedSpecs = specs;
         }
 
-        return this._cachedSpecs;
+        for (var i = 0; i < this._modifiers.length; i++) {
+            var spec = this._modifiers[i].modify({
+                target: this._items[i].render()
+            });
+
+            specs.push(spec);
+        }
+
+        return specs;
     };
 
     module.exports = FlexGrid;
